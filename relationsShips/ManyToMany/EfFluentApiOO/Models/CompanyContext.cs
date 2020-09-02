dad51beda4cb165cdbb6d8d42d3e48968d698002
selman_ofis <selmantunc@hotmail.com>
2020-09-02 15:54:35 +0300

    add

---
+++ relationsShips/ManyToMany/EfFluentApiOO/Models/CompanyContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+
+namespace EfFluentApiOO.Models
+{
+    public class CompanyContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public Microsoft.EntityFrameworkCore.DbSet<City> City { get; set; }
+
+        protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Write Fluent API configurations here
+
+           modelBuilder.Entity<City>().HasOne(e => e.CityInformation).WithOne(e => e.City).HasForeignKey<City>(e => e.CityInformationId);
+        }
+    }
+}
