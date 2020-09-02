76afca5f019ad3d1fa0c0f7ef6782ed1a873ea46
selman_ofis <selmantunc@hotmail.com>
2020-09-02 12:11:49 +0300

    log example add

--- EfCoreSamples.Logging-example/EfCoreSamples.Logging.Web/Startup.cs
+++ EfCoreSamples.Logging-example/EfCoreSamples.Logging.Web/Startup.cs
@@ -64,7 +64,7 @@ namespace EfCoreSamples.Logging.Web
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+               // app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
